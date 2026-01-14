d1c7ae587c0c9013bc667222dae69de361fde0be
Asta-V22 <vatsarudransh5854@gmail.com>
2026-01-14 23:39:30 +0530

    Time: 16 ms (8.01%) | Memory: 15.2 MB (5.92%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_map<ListNode*,int> mpp;
+
+        ListNode* temp = head;
+
+        while(temp){
+            if(mpp.find(temp)==mpp.end()){
+                mpp[temp]=1;
+                temp = temp->next;
+            }
+            else{
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
