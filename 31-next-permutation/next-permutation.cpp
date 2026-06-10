d0c875ef68a809d76d3187c33e30ac5672ca49e9
Asta-V22 <vatsarudransh5854@gmail.com>
2026-06-10 19:31:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (66.54%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int index = -1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+
+        if(index == -1){
+            sort(nums.begin(), nums.end());
+        }
+        else{
+            for(int i = n-1; i>index; i--){
+                if(nums[i]>nums[index]){
+                    swap(nums[i],nums[index]);
+                    break;
+                }
+            }
+            //sort the rest of the part of the array
+            sort(nums.begin()+index+1, nums.end());
+        }
+
+    }
+};
