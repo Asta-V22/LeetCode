832f11abdf95a8f2ee7a5fd4879dfa91a8ff1bdc
Rudransh Vatsa <vatsarudransh5854@gmail.com>
2026-07-21 01:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (26.14%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -3,39 +3,26 @@ public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         int m = grid.size();
         int n = grid[0].size();
-        int totalElements = m*n;
 
-        
+        int total = m*n;
 
-        k = k%totalElements;   //this will give the shift number
-        if(k==0) return grid;
+        vector<vector<int>> duplicate(m,vector<int>(n));
 
-        //lets make a duplicate matrix that will store the new values
-        vector<vector<int>> duplicate(m, vector<int>(n));
+        //let's loop throught the grid to calculate the newIndexes and put them in the grid
 
-        for(int x=0; x<k; x++){
-            //shift by one place everytime
-            for(int i=0; i<m; i++){
-                for(int j=0; j<n; j++){
-                    if(i==m-1 && j==n-1){
-                        duplicate[0][0]=grid[i][j];
-                    }
-                    else if(j==n-1){
-                        duplicate[i+1][0]= grid[i][j];
-                    }
-                    else{
-                        duplicate[i][j+1]=grid[i][j];
-                    }
-                }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                int oldIndex = i*n + j;
+                int newIndex = (oldIndex + k) % total;
+
+                int newRow = newIndex/n;
+                int newCol = newIndex%n;
+                
+
+                duplicate[newRow][newCol] = grid[i][j];
             }
-
-            grid=duplicate;
-
-
         }
 
-        return grid;
-
-
+        return duplicate;
     }
 };
