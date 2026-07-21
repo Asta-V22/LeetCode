5786ce55bac8e0599058eed1786735d5901ba006
Rudransh Vatsa <vatsarudransh5854@gmail.com>
2026-07-21 14:37:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (54.84%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     bool isPalindrome(string s){
-
-    for(int i = 0; i<s.size(); i++){
-        if(!isalnum((unsigned char)s[i])){
-            s.erase(i,1);
-            i--;
-        }
-    }
-
-
-
-    for(auto& c: s  ){            //by putting & it will change the content of the string
-        c = tolower(c);
-    }  
-
+    
     int start = 0;
-    int end = s.size() - 1;
+    int end = s.size()-1;
 
     while(start<end){
-        if(s[start] == s[end]){
+        while(start<end && !isalnum(s[start])){
             start++;
+        }
+        while(start<end && !isalnum(s[end])){
             end--;
         }
-        else{
+
+        if(tolower(s[start])!=tolower(s[end])){
             return false;
         }
+
+        start++;
+        end--;
     }
 
     return true;
-    
-    
 }
 
 };
