b3b6bd3ab50ba9b51afbc82b0c687d44d8203ecd
Rudransh Vatsa <vatsarudransh5854@gmail.com>
2026-08-01 19:52:52 +0530

    Time: 11 ms (13.68%) | Memory: 25.5 MB (20.74%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -9,18 +9,28 @@ public:
             for(int j=0; j<9; j++){
                 if(board[i][j]!='.'){
                     //check if this number has came before in the same row
-                    if(rows[i].count(board[i][j])) return false;
-                    rows[i].insert(board[i][j]);
+
+                    //commented lines also work but the current one is a smarter one
+
+                    //
+                    // if(rows[i].count(board[i][j])) return false;
+                    // rows[i].insert(board[i][j]);
+
+                    if(!(rows[i].insert(board[i][j]).second)) return false;
 
                     //check if this number has came before in the same cols
-                    if(cols[j].count(board[i][j])) return false;
-                    cols[j].insert(board[i][j]);
+                    // if(cols[j].count(board[i][j])) return false;
+                    // cols[j].insert(board[i][j]);
+
+                    if(!(cols[j].insert(board[i][j]).second)) return false;
 
                     //check if this number has came anywhere in the box before
                     int numBox = (i/3)*3 + (j/3);
 
-                    if(boxs[numBox].count(board[i][j])) return false;
-                    boxs[numBox].insert(board[i][j]);
+                    // if(boxs[numBox].count(board[i][j])) return false;
+                    // boxs[numBox].insert(board[i][j]);
+
+                    if(!(boxs[numBox].insert(board[i][j]).second)) return false;
                 }
             }
         }
