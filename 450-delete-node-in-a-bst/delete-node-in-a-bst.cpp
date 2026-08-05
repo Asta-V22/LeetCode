253a856823cf628e595ab7e646535f583159e85c
Rudransh Vatsa <vatsarudransh5854@gmail.com>
2026-08-05 16:32:25 +0530

    Time: 0 ms (100.00%) | Memory: 34.4 MB (52.53%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* righty(TreeNode* root){
+        while(root->right!=NULL){
+            root=root->right;
+        }
+        return root;
+    }
+    TreeNode* helper(TreeNode* root){
+        //we have three cases
+        //1. The node at which our key was only has right side
+        if(root->left==NULL){   //now lets assume if we had root->right==NULL also then it will reurn root->right(which will be null) so that also works out
+            return root->right;
+        }
+        else if(root->right==NULL){
+            return root->left;
+        }
+        TreeNode* rightSide = root->right;
+        TreeNode* rightmost = righty(root->left);
+        rightmost->right = rightSide;
+
+        return root->left;
+    }
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL) return root;
+
+        if(root->val==key) return helper(root);
+
+        //store root for the returning purposes
+        TreeNode* dummy = root;
+
+        while(root!=NULL){
+            if(root->val>key){
+                if(root->left!=NULL && root->left->val==key){
+                    root->left = helper(root->left); //this helper function will care about all the rearrangements
+                    break;
+                }
+                else{
+                    //doubt: What happens if the root->left becomes null and we don't get our answer??
+                    root = root->left;
+                }
+            }
+            else{
+                if(root->right!=NULL && root->right->val==key){
+                    root->right = helper(root->right);
+                    break;
+                }
+                else{
+                    root = root->right;
+                }
+            }
+        }
+        return dummy;
+
+
+    }
+};
