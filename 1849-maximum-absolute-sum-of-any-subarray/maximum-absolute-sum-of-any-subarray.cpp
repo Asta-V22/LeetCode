f4db44372301b90573e5ca0973d7a4fcf5138b98
Rudransh Vatsa <vatsarudransh5854@gmail.com>
2026-08-15 14:58:09 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (62.32%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,30 +1,23 @@
 class Solution {
 private:
-    int maxSubarraySum(vector<int> nums){
-        int currSubSum = nums[0];
+    int maxSubarraySum(vector<int> &nums){
+        int currMaxSubSum = nums[0];
+        int currMinSubSum = nums[0];
         int maxSubSum = nums[0];
-
-        for(int i=1; i<nums.size(); i++){
-            currSubSum = max(nums[i], nums[i]+currSubSum);
-            maxSubSum = max(maxSubSum, currSubSum);
-        }
-
-        return maxSubSum;
-    }
-
-    int minSubarraySum(vector<int> nums){
-        int currSubSum = nums[0];
         int minSubSum = nums[0];
 
         for(int i=1; i<nums.size(); i++){
-            currSubSum = min(nums[i], nums[i]+currSubSum);
-            minSubSum = min(minSubSum, currSubSum);
+            currMaxSubSum = max(nums[i], nums[i]+currMaxSubSum);
+            currMinSubSum = min(nums[i], nums[i]+currMinSubSum);
+            maxSubSum = max(maxSubSum, currMaxSubSum);
+            minSubSum = min(minSubSum, currMinSubSum);
         }
 
-        return minSubSum;
+        return max(abs(maxSubSum), abs(minSubSum));
     }
+
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        return max(abs(minSubarraySum(nums)), abs(maxSubarraySum(nums)));
+        return maxSubarraySum(nums);
     }
 };
