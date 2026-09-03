25c56a06539ec79c554df284a066050bf4a43a4f
Rudransh Vatsa <vatsarudransh5854@gmail.com>
2026-09-03 23:58:31 +0530

    Time: 3 ms (72.51%) | Memory: 165.7 MB (87.43%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -6,11 +6,30 @@ public:
         //observation: if the first element is odd (in sorted array) then it doesn't matter what comes after it, we can change it into odd array, but if the first element is even then the entire must be even ahead, 
         //4,6,7,9,12,15
 
-        sort(nums1.begin(), nums1.end());
-        if(nums1[0]%2==0){
-            for(int i=1; i<nums1.size(); i++){
-                if(nums1[i]%2!=0) return false;
-            }
+        //MY ORIGINAL SOLUTION O(nlogn)
+
+        // sort(nums1.begin(), nums1.end());
+        // if(nums1[0]%2==0){
+        //     for(int i=1; i<nums1.size(); i++){
+        //         if(nums1[i]%2!=0) return false;
+        //     }
+        // }
+        // return true;
+
+
+        //OPTIMIZED O(N) approach:
+        //we only care about the mininum value in the array and if any odd exists in the array or not, so we'll do exactly that without sorting
+
+        int min_element = INT_MAX;
+        bool hasOdd =false;
+
+        for(int i=0; i<nums1.size(); i++){
+            min_element = min(min_element, nums1[i]);
+            if(nums1[i]%2==1) hasOdd = true;
+        }
+
+        if(min_element%2==0 && hasOdd){
+            return false;
         }
         return true;
     }
