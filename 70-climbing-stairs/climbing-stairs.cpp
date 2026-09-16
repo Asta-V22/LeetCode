5913afd5c1630fbbc4780589616a9b22e495ea3d
Rudransh Vatsa <vatsarudransh5854@gmail.com>
2026-09-16 14:55:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.30%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,21 +1,20 @@
-/*-----------------DP WITH MEMOIZANTION------------------------*/
-
+/*---------BOTTOM UP----------------*/
 
 class Solution {
 public:
-    int solve(int i, int n, int &ways, vector<int> &dp){
-        //i is how many steps we have already covered
-        if(i==n){
-            ways++;
-            return 1;
-        }
-        if(i>n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        return dp[i] = solve(i+1, n, ways,dp) + solve(i+2,n,ways,dp);
-    }
     int climbStairs(int n) {
-        vector<int> dp(n,-1);
-        int ways = 0;
-        return solve(0,n,ways,dp);
+        if(n<=2) return n;
+        vector<int> vec(n+1);
+
+        //number of ways to climb the 0th and 1st stairs
+        vec[0] = 0;
+        vec[1] = 1;
+        vec[2] = 2;
+
+        for(int i=3; i<=n; i++){
+            vec[i] = vec[i-1]+vec[i-2];
+        }
+
+        return vec[n];
     }
 };
